177d8c78672b06376aa06533defca1cf2d1d772e
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2022-12-15 11:10:39 -0300

    commit
    commit

--- feature_social_media/src/main/java/com/example/feature_social_media/instagram/InstaProfileScreen.kt
+++ feature_social_media/src/main/java/com/example/feature_social_media/instagram/InstaProfileScreen.kt
@@ -7,14 +7,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import br.com.vtvinicius.uikit.utils.extensions.VerticalSpacer
 import com.example.feature_social_media.instagram.components.*
 
 @Composable
 fun InstaProfileScreen() {
+
+    var selectedTabIndex by remember {
+        mutableStateOf(0)
+    }
 
     Scaffold(
         topBar = {
@@ -36,6 +41,36 @@ fun InstaProfileScreen() {
                 VerticalSpacer(height = 16)
                 ButtonsMiddleRow()
                 DestaquesRow()
+                PostTabView(
+                imageWithTexts = listOf(
+                    ImageWithText(
+                        image = painterResource(id = br.com.vtvinicius.uikit.R.drawable.ic_grid),
+                        text = "Posts"
+                    ),
+                    ImageWithText(
+                        image = painterResource(id =  br.com.vtvinicius.uikit.R.drawable.ic_reels),
+                        text = "Reels"
+                    ),
+                    ImageWithText(
+                        image = painterResource(id =  br.com.vtvinicius.uikit.R.drawable.ic_tag),
+                        text = "Profile"
+                    ),
+                )) {
+                    selectedTabIndex = it
+                }
+
+                when(selectedTabIndex) {
+                    0 -> PostSection(
+                        posts = listOf(
+                            painterResource(id = br.com.vtvinicius.uikit.R.drawable.img_vini_1),
+                            painterResource(id = br.com.vtvinicius.uikit.R.drawable.img_vini_2),
+                            painterResource(id = br.com.vtvinicius.uikit.R.drawable.img_vini_3),
+                            painterResource(id = br.com.vtvinicius.uikit.R.drawable.img_vini_4),
+                            painterResource(id = br.com.vtvinicius.uikit.R.drawable.img_vini_5),
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             }
 
         })
