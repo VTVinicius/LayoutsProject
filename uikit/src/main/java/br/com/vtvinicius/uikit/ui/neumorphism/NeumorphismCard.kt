3e2e5519d65cb0dea5bf787a857976eb7a1d5b18
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2023-04-16 17:45:27 -0300

    Neo

--- uikit/src/main/java/br/com/vtvinicius/uikit/ui/neumorphism/NeumorphismCard.kt
+++ uikit/src/main/java/br/com/vtvinicius/uikit/ui/neumorphism/NeumorphismCard.kt
@@ -2,24 +2,30 @@ package br.com.vtvinicius.uikit.ui.neumorphism
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -83,36 +89,52 @@ fun NeumorphicButton2(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     shape: Shape = RoundedCornerShape(12.dp),
-    backgroundColor: Color = Color.LightGray,
     contentPadding: PaddingValues = PaddingValues(20.dp),
+    onPressed: () -> Unit = {},
+    onReleased: () -> Unit = {},
     content: @Composable() () -> Unit,
 ) {
-    val elevation = 4.dp
-    val shadowColorLight = Color.White
-    val shadowColorDark = Color.White
+
+    val backgroundColor = remember { mutableStateOf(Color.LightGray) }
+    val borderColor = remember { mutableStateOf(Color.Gray) }
 
     val backgroundModifier = Modifier
-        .shadow((-2).dp, shape, false, shadowColorLight, shadowColorLight)
+        .shadow((-2).dp, shape, false, Color.White, Color.White)
 
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
 
+    if (isPressed) {
+        onPressed()
+        backgroundColor.value = Color.White
+        borderColor.value = Color.LightGray
+    } else {
+        onReleased()
+        backgroundColor.value = Color.White
+        borderColor.value = Color.White
+    }
 
     androidx.compose.material.Button(
-        onClick = onClick,
         modifier = modifier.then(backgroundModifier),
+        onClick = onClick,
+      
         shape = shape,
         colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.White,
+            backgroundColor = backgroundColor.value,
             contentColor = Color.Black
+
         ),
+        border = BorderStroke(5.dp, color = borderColor.value),
         contentPadding = contentPadding,
         elevation = ButtonDefaults.elevation(
-            defaultElevation = 30.dp,
+            defaultElevation = 20.dp,
             pressedElevation = 0.dp,
             disabledElevation = 0.dp,
-            hoveredElevation = 30.dp,
+            hoveredElevation = 20.dp,
             focusedElevation = 1.dp,
-        )
-    ) {
+        ),
+
+        ) {
         content()
     }
 }
@@ -139,6 +161,7 @@ fun NeumorphicButton(
                 RoundedCorner(8.dp),
             )
         ),
+
         colors = ButtonDefaults.buttonColors(
             backgroundColor = backgroundColor,
             contentColor = Color.Black
@@ -194,25 +217,14 @@ fun NeumorphicButtonSample2() {
         NeumorphicButton2(
             onClick = { /* Do something on click */ },
             contentPadding = PaddingValues(16.dp),
-            backgroundColor = Color(0xFFF0F0F0)
-        ) {
-            Text("Neumorphic Button", textAlign = TextAlign.Center, modifier = Modifier.padding(1.dp))
-        }
-    }
-}
-
-@Composable
-fun NeumorphicButtonSample3() {
-    Box(
-        contentAlignment = Alignment.Center
-    ) {
-        NeumorphicButton2(
-            onClick = { /* Do something on click */ },
-            contentPadding = PaddingValues(16.dp),
-            backgroundColor = Color(0xFFF0F0F0)
-        ) {
-            Text("Neumorphic Button 2", textAlign = TextAlign.Center)
-        }
+            content = {
+                Text(
+                    "Neumorphic Button",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(1.dp)
+                )
+            }
+        )
     }
 }
 
@@ -269,7 +281,7 @@ fun NeumorphicButtonSamplePreviewLib() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        NeumorphicButton2(onClick = {}, content = { Text(text = "Neomorfismo")} )
+        NeumorphicButton2(onClick = {}, content = { Text(text = "Neomorfismo") })
     }
 
 }
